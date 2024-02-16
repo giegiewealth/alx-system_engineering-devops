87dd075402814d4a8f2ebf2914d782ac2a0760bd
root <angelamarfo17@gmail.com>
2024-02-16 14:09:07 -0800

    completed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""This script queries the REDDIT API"""
+"subscribers count"
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    This function queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for
-    a given subreddit.
-    """
-    base_url = 'https://www.reddit.com'
-    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus\
-                5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko)\
-                Chrome/107.0.0.0 Mobile Safari/537.36'}
-
-    data = requests.get("{}/r/{}/about.json"
-                        .format(base_url, subreddit), headers=headers,
-                        allow_redirects=False)
-    if data.status_code != 200:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code != 200:
         return 0
-    return data.json().get('data').get('subscribers')
+    else:
+        return data['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-"""This script queries the REDDIT API"""
-
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """
-    This function prints the titles of the first
-    10 hot posts listed for a given subreddit.
-    """
-    base_url = 'https://www.reddit.com'
-    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus\
-                5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko)\
-                Chrome/107.0.0.0 Mobile Safari/537.36'}
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    data = requests.get("{}/r/{}/hot.json"
-                        .format(base_url, subreddit), headers=headers,
-                        allow_redirects=False)
-    if data.status_code != 200:
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
+        return
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
         print(None)
     else:
-        result = data.json().get('data').get('children')
-        for i in range(0, 10):
-            print(result[i].get('data').get('title'))
+        for post in hot_posts:
+            print(post['data']['title'])
